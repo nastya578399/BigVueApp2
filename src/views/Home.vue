c4fe34d719cc1fbbab2c62162ffb317f8e49af76
Romanova Nastya <opt57839@mail.ru>
2021-09-28 08:12:30 +0300

    Создание главной страницы

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,37 @@
 <template>
-  <div class="home">
-  </div>
+  <app-page title="Список заявок">
+    <template #header>
+      <button class="btn primary" @click="modal = true">Создать</button>
+    </template>
+
+    <request-table :requests="[]"></request-table>
+
+
+    <teleport to="body">
+      <app-modal v-if="modal" title="Создать заявку" @close="modal = false">
+        <RequestModal />
+      </app-modal>
+    </teleport>
+  </app-page>
 </template>
 
 <script>
+import {ref} from 'vue'
+import AppPage from '../components/ui/AppPage.vue'
+import RequestTable from '../components/request/RequestTable.vue'
+import AppModal from '../components/ui/AppModal.vue'
+import RequestModal from '../components/request/RequestModal'
+
 export default {
-  name: 'Home',
+  setup() {
+    const modal = ref(false)
+
+    return {
+      modal
+    }
+  },
+  components: {AppPage, RequestTable, AppModal, RequestModal}
 }
 </script>
+,
+    RequestModal
